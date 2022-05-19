d888c43c80eee0c732c207d409f2175c44581288
kimseungki <skkim0832@naver.com>
2022-05-20 02:32:01 +0900

    Update N과 M （2）.java

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,47 +1,43 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] arr;
-	static int N, M;
-	static boolean[] visit;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringBuilder sb = new StringBuilder();
+    static int N, M, start;
+    static int[] isSelected, isUsed;
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
+    public static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isSelected = new int[M + 1];
+    }
+    public static void output() throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 
-		StringTokenizer st = null;
+    public static void func(int limitNum) {
+        if (limitNum == M + 1) {
+            for (int index = 1; index <= M; index++) {
+                sb.append(isSelected[index]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int index = isSelected[limitNum - 1] + 1; index <= N; index++) {
+                isSelected[limitNum] = index;
+                func(limitNum + 1);
+                isSelected[limitNum] = 0;
+            }
+        }
+    }
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[M];
-		visit = new boolean[N];
-		dfs(0, 0);
-		bw.write(sb + "\n");
-		bw.flush();
-		bw.close();
-	}
-
-	private static void dfs(int base, int count) throws IOException {
-		if (count == M) {
-			for (int val : arr) {
-				// bw.write(val+" ");
-				sb.append(val).append(" ");
-			}
-			// bw.write("\n");
-			sb.append("\n");
-			return;
-		}
-		for (int i = base; i < N; i++) {
-			arr[count] = i + 1;
-			dfs(i + 1, count + 1);
-		}
-
-	}
+    public static void main(String[] args) throws Exception {
+        input();
+        func(1);
+        output();
+    }
 }
