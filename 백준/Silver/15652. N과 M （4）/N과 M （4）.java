14ad71c1daf49e453fef7c0c8e13f3523b68ea4e
kimseungki <skkim0832@naver.com>
2022-05-20 02:33:12 +0900

    Update N과 M （4）.java

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,52 +1,41 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] num;
-	static int N;
-	static int limit;
-	static boolean[] visited;
-	static int[] store;
-	static String str;
-	static StringBuilder sb;
+    static int N, M, start;
+    static int[] isSelected, isUsed;
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		str = null;
-		N = Integer.parseInt(st.nextToken()); // 수
-		limit = Integer.parseInt(st.nextToken()); // 제한된 수
-		num = new int[limit];
-		visited = new boolean[N + 1];
-		sb = new StringBuilder();
-		dfs(0, "",0);
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isSelected = new int[M + 1];
+    }
 
-		bw.write(sb.toString().trim());
-		bw.close();
-	}
+    public static void func(int limitNum) {
+        if (limitNum == M + 1) {
+            for (int index = 1; index <= M; index++) {
+                sb.append(isSelected[index]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            start = isSelected[limitNum - 1];
+            if (start == 0) {
+                start = 1;
+            }
+            for (int index = start; index <= N; index++) {
+                isSelected[limitNum] = index;
+                func(limitNum + 1);
+                isSelected[limitNum] = 0;
+            }
+        }
+    }
 
-	private static void dfs(int index, String str,int last) {
-		if (index == limit) {
-			sb.append(str.trim() + "\n");
-			// System.out.println();
-			return;
-		}
-		for (int j = 1; j <= N; j++) {
-			if (visited[j] == false && j>=last) {
-				num[index] = j;
-				last = j;
-				// visited[j]=true;
-					dfs(index + 1, str + " " + j, last);
-
-				// visited[j]=false;
-			}
-		}
-
-	}
-
+    public static void main(String[] args) throws Exception {
+        input();
+        func(1);
+        System.out.println(sb.toString());
+    }
 }
