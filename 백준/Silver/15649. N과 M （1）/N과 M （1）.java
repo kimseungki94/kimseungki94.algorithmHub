17ba1418ead246ef339f459979b4f3fbd71a5708
kimseungki <skkim0832@naver.com>
2022-05-20 02:30:24 +0900

    Update N과 M （1）.java

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,49 +1,44 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] arr;
-	static int N,M;
-	static boolean[] visit;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		
-		StringTokenizer st = null;
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[M];
-		visit = new boolean[N];
-		dfs(0);
-		bw.write(sb+"\n");
-		bw.flush();
-		bw.close();
-	}
+    static int N, M;
+    static int[] isSelected, isUsed;
+    static StringBuilder sb = new StringBuilder();
 
-	private static void dfs(int count) throws IOException {
-		if(count==M) {
-			for(int val : arr) {
-				//bw.write(val+" ");
-				sb.append(val).append(" ");
-			}
-			//bw.write("\n");
-			sb.append("\n");
-			return;
-		}
-		for(int i=0;i<N;i++) {
-			if(visit[i]==false) {
-				visit[i]=true;
-				arr[count]=i+1;
-				dfs(count+1);
-				visit[i]=false;
-			}
-		}
-	}
+    public static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isSelected = new int[M + 1];
+        isUsed = new int[N + 1];
+    }
+
+    public static void func(int limitNum) {
+        if (limitNum == M + 1) {
+            for (int index = 1; index <= M; index++) {
+                sb.append(isSelected[index]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int index = 1; index <= N; index++) {
+                if (isUsed[index] == 1) {
+                    continue;
+                }
+                isSelected[limitNum] = index;
+                isUsed[index] = 1;
+                func(limitNum + 1);
+                isSelected[limitNum] = 0;
+                isUsed[index] = 0;
+
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        func(1);
+        System.out.println(sb.toString());
+    }
 }
