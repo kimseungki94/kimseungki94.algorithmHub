90f5f04cd39752536da7c69893b26e0105893782
kimseungki <skkim0832@naver.com>
2022-05-25 16:27:07 +0900

    [Silver II] Title: 로또, Time: 88 ms, Memory: 13268 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st = null;
+    static int[] arr;
+    static boolean[] visit;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        while(true){
+            st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            if(N==0){
+                break;
+            }
+            arr = new int[N];
+            visit = new boolean[N];
+            for(int i=0;i<N;i++) {
+                arr[i]=Integer.parseInt(st.nextToken());
+            }
+            dfs(0,0);
+            sb.append("\n");
+
+        }
+        bw.write(sb+"\n");
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs(int start, int count) {
+
+        if(count==6){
+            for(int i=0;i<N;i++){
+                if(visit[i]==true){
+                    sb.append(arr[i]+" ");
+                }
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start;i<N;i++){
+            visit[i]=true;
+            dfs(i+1,count+1);
+            visit[i]=false;
+        }
+    }
+
+
+}
