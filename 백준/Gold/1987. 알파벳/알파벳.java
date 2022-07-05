d4c8580966945d65361930367512fcf59c3be5e0
kimseungki <skkim0832@naver.com>
2022-07-05 18:40:43 +0900

    [Gold IV] Title: 알파벳, Time: 1284 ms, Memory: 15544 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,76 +1,57 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
-    static int R, C, max;
-    static int[][] map, isSelected;
-    static int[] isNumbers;
-    static StringBuilder sb;
 
-    public static void input() throws Exception {
+    static int N, M, K, count, size, max;
+    static StringBuilder sb = new StringBuilder();
+    static Deque<Character> deque = new LinkedList<>();
+    static int[][] map, tempMap;
+    static int[] isVieited = new int[26];
+    static int[] dy = new int[]{-1, 1, 0, 0};
+    static int[] dx = new int[]{0, 0, -1, 1};
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public void input() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        map = new int[R + 1][C + 1];
-        isSelected = new int[R + 1][C + 1];
-        isNumbers = new int[R * C + 1];
-        for (int i = 1; i <= R; i++) {
-            String text = br.readLine();
-            for (int j = 1; j <= C; j++) {
-                map[i][j] = text.charAt(j - 1) - 'A' + 1;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String text = st.nextToken();
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = text.charAt(j - 1) - 'A';
             }
         }
-        max = Integer.MIN_VALUE;
     }
 
-    public static boolean isValidate(int row, int col, int limit) {
-        if (limit == 1) {
-            return true;
-        }
-        if (isSelected[row][col] == 1) {
-            return false;
-        }
-        for (int i = 1; i < limit; i++) {
-            if (isNumbers[i] == map[row][col]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void func(int row, int col, int limit) {
-        if (limit == R * C + 1) {
-            max = Math.max(max, limit-1);
+    public void DFS(int row, int col, int limit) {
+        if(isVieited[map[row][col]]==1) {
+            max = Math.max(limit,max);
             return;
-        } else {
-            if (isValidate(row, col, limit)) {
-                isSelected[row][col] = 1;
-                isNumbers[limit] = map[row][col];
-                if (row - 1 > 0) {
-                    func(row - 1, col, limit + 1);
-                }
-                if (row + 1 < R + 1) {
-                    func(row + 1, col, limit + 1);
-                }
-                if (col - 1 > 0) {
-                    func(row, col - 1, limit + 1);
-                }
-                if (col + 1 < C + 1) {
-                    func(row, col + 1, limit + 1);
-                }
-                isSelected[row][col] = 0;
-            } else {
-                max = Math.max(max, limit-1);
+        }
+        for (int i = 0; i < 4; i++) {
+            int y = row + dy[i];
+            int x = col + dx[i];
+            isVieited[map[row][col]]=1;
+            if (y >= 1 && y <= N && x >= 1 && x <= M) {
+                DFS(y,x,limit+1);
             }
+            isVieited[map[row][col]]=0;
         }
     }
 
     public static void main(String[] args) throws Exception {
-        input();
-        func(1, 1, 1);
+        Main main = new Main();
+        main.input();
+        main.DFS(1, 1,0);
         System.out.println(max);
     }
 }
