99b9eea27eef5e38c7f2e973542268c2c27cef9f
kimseungki <skkim0832@naver.com>
2022-08-22 11:29:53 +0900

    [Silver I] Title: 영역 구하기, Time: 152 ms, Memory: 16244 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,72 +1,67 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 public class Main {
 
-    static int N, M, K, count, size;
     static StringBuilder sb = new StringBuilder();
-    static Deque<Character> deque = new LinkedList<>();
-    static int[][] map,tempMap;
+    static int N, M, K, count, T, max, min, maxValue;
+    static int[][] map,arr;
+    static boolean[][] isSelected;
     static int[] dy = new int[]{-1, 1, 0, 0};
     static int[] dx = new int[]{0, 0, -1, 1};
-    static ArrayList<Integer> list = new ArrayList<>();
+    static List<Integer> list = new ArrayList<>();
 
-    public void input() throws Exception {
+
+    public void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        map = new int[N + 1][M + 1];
-        for (int i = 0; i < K; i++) {
+        map = new int[N][M];
+        for(int t=0;t<K;t++) {
             st = new StringTokenizer(br.readLine());
-            int startX = Integer.parseInt(st.nextToken()) + 1;
-            int startY = Integer.parseInt(st.nextToken()) + 1;
-            int endX = Integer.parseInt(st.nextToken());
-            int endY = Integer.parseInt(st.nextToken());
-            for (int row = startY; row <= endY; row++) {
-                for (int col = startX; col <= endX; col++) {
-                    map[row][col] = 1;
+            int X1 = Integer.parseInt(st.nextToken());
+            int Y1 = N-Integer.parseInt(st.nextToken());
+            int X2 = Integer.parseInt(st.nextToken());
+            int Y2 = N-Integer.parseInt(st.nextToken());
+
+            for(int i=Y2;i<Y1;i++) {
+                for(int j=X1;j<X2;j++) {
+                    map[i][j]=1;
                 }
             }
         }
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<M;j++) {
+                if(map[i][j]==0){
+                    count=0;
+                    DFS(i,j);
+                    list.add(count);
+                }
+            }
+        }
+        Collections.sort(list);
     }
 
     public void DFS(int row, int col) {
-        size++;
         map[row][col] = 1;
+        count++;
         for (int i = 0; i < 4; i++) {
-            int y = row + dy[i];
-            int x = col + dx[i];
-            if (y >= 1 && y <= N && x >= 1 && x <= M && map[y][x] == 0) {
-                DFS(y, x);
-            }
+            int Y = row + dy[i];
+            int X = col + dx[i];
+            if (Y < 0 || Y >= N || X < 0 || X >= M) continue;
+            if (map[Y][X] == 0) DFS(Y, X);
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         Main main = new Main();
         main.input();
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                size = 0;
-                if (map[i][j] == 0) {
-                    count++;
-                    main.DFS(i, j);
-                    list.add(size);
-                }
-            }
-        }
-        sb.append(count+"\n");
-        Collections.sort(list);
-        for(int i : list) {
-            sb.append(i+" ");
-        }
+        System.out.println(list.size());
+        for(int value : list) sb.append(value+" ");
         System.out.println(sb.toString());
     }
 }
