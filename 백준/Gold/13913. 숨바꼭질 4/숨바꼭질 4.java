ad1778e7b93353cfb06b2e3b46a8ec79685c4a6a
kimseungki <skkim0832@naver.com>
2022-09-01 23:59:14 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 848 ms, Memory: 301236 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M, limit, size, min, count;
+    static String answerLoad;
     static int[] isVisit;
     static Queue<Point> queue = new LinkedList<>();
     static StringBuilder sb = new StringBuilder();
@@ -15,12 +15,12 @@ public class Main {
     public static class Point {
         int value;
         int time;
-        String text;
+        String load;
 
-        public Point(int value, int time, String text) {
+        public Point(int value, int time, String load) {
             this.value = value;
             this.time = time;
-            this.text = text;
+            this.load = load;
         }
     }
 
@@ -29,38 +29,46 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        min = Integer.MAX_VALUE;
         isVisit = new int[100001];
         if (N >= M) {
-            sb.append(N-M+"\n");
-            while (N>=M) sb.append(N--+" ");
+            System.out.println(N - M);
+            for(int i=N;i>=M;i--) {
+                sb.append(i).append(" ");
+            }
+            System.out.println(sb.toString());
         } else {
             BFS();
+            System.out.println(min);
+            System.out.println(answerLoad);
         }
-        System.out.println(sb.toString());
     }
 
-    public static void BFS() {
-        queue.offer(new Point(N, 0, N + " "));
+    public static int BFS() {
+        queue.offer(new Point(N,0,N+" "));
         while (!queue.isEmpty()) {
             Point p = queue.poll();
             int value = p.value;
             int time = p.time;
-            String text = p.text;
-            isVisit[value] = 1;
-            if (value == M) {
-                sb.append(time+"\n");
-                sb.append(text);
-                return;
+            String load = p.load;
+            isVisit[value]=1;
+            if(value==M) {
+                if(min>time) {
+                    min=time;
+                    answerLoad=load;
+                    return 0;
+                }
             }
-            for (int j = 1; j <= 3; j++) {
-                int data = outValue(value, j);
-                if (data < 0 || data > 100000) continue;
-                if (isVisit[data] == 0) {
-                    String textData = text+data+" ";
-                    queue.offer(new Point(data, time + 1, textData));
+
+            for(int i=1;i<=3;i++) {
+                int editValue=outValue(value,i);
+                if(editValue<0 || editValue>100000) continue;
+                if(isVisit[editValue]==0) {
+                    queue.add(new Point(editValue,time+1,load+editValue+" "));
                 }
             }
         }
+        return 0;
     }
 
     public static int outValue(int value, int button) {
@@ -69,4 +77,5 @@ public class Main {
         if (button == 3) return value * 2;
         return 1;
     }
+
 }
