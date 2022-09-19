5974914c5d2ae6a80a0e598aef70dd33afeab58d
kimseungki <skkim0832@naver.com>
2022-09-19 13:41:03 +0900

    [Silver II] Title: 스타트와 링크, Time: 344 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,69 +1,68 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 public class Main {
+    static int N, M, K, max, sum, min;
+    static int[][] map;
+    static boolean[] isChecked;
 
-    static int N, M, K, count, size, max, sum, min;
-    static StringBuilder sb = new StringBuilder();
-    static int[][] map, tempMap;
-    static int[] arr;
-    static boolean flag = false;
-    static ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
+        input();
+        Solution();
+        System.out.println(min);
+    }
 
-    public void input() throws Exception {
+
+    public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        map = new int[N + 1][N + 1];
         min = Integer.MAX_VALUE;
-        for (int i = 1; i <= N; i++) {
+        map = new int[N][N];
+        isChecked = new boolean[N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                int value = Integer.parseInt(st.nextToken());
-                map[i][j] = value;
-                sum += value;
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        arr = new int[N + 1];
     }
 
-    public void DFS(int limit, int index) {
-        if (min == 0) return;
-        if (limit == N / 2) {
-            int data = 0;
-            int data2 = 0;
-            for (int i = 1; i < N; i++) {
-                for (int j = i + 1; j <= N; j++) {
-                    if (arr[i] == 1 && arr[j] == 1) {
-                        data += map[i][j] + map[j][i];
-                    }
-                    if (arr[i] == 0 && arr[j] == 0) {
-                        data2 += map[i][j] + map[j][i];
-                    }
-                }
+    public static void Solution() {
+        DFS(0, 0);
+    }
 
-            }
-            min = Math.min(min, Math.abs(data - data2));
+    public static void DFS(int index, int limit) {
+        if (limit == N / 2) {
+            findDiff();
             return;
         }
-        for (int i = index; i <= N; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-                DFS(limit + 1, i + 1);
-                arr[i] = 0;
+        for (int i = index; i < N; i++) {
+            if (!isChecked[i]) {
+                isChecked[i] = true;
+                DFS(i + 1, limit + 1);
+                isChecked[i] = false;
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        Main main = new Main();
-        main.input();
-        main.DFS(0, 1);
-        System.out.println(min);
+    public static void findDiff() {
+        int value = 0;
+        int diffValue = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                if (i == j) continue;
+                if (isChecked[i] && isChecked[j]) {
+                    value += map[i][j];
+                    value += map[j][i];
+                }
+                if (!isChecked[i] && !isChecked[j]) {
+                    diffValue += map[i][j];
+                    diffValue += map[j][i];
+                }
+            }
+        }
+        min = Math.min(min, Math.abs(value - diffValue));
     }
 }
