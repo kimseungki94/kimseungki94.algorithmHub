2f11c6b8006ba58bcb6cf7ae4ec42b1c3ae9cddd
kimseungki <skkim0832@naver.com>
2022-09-30 16:12:01 +0900

    [Silver I] Title: 기타 레슨, Time: 340 ms, Memory: 24292 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, min, sum;
+    static int N, M, min, sum,max;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
@@ -21,27 +21,26 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             sum += arr[i];
+            max=Math.max(arr[i],max);
         }
     }
 
     public static void Solution() {
         int lt, rt, mid;
-        lt = 0;
+        lt = max;
         rt = sum;
         while (lt <= rt) {
             mid = (lt + rt) / 2;
             int value = 1;
             int countSum = 0;
-            boolean flag = false;
             for (int i = 0; i < N; i++) {
                 if (countSum + arr[i] > mid) {
                     value++;
                     countSum = 0;
                 }
                 countSum += arr[i];
-                if(arr[i]>mid) flag=true;
             }
-            if (value > M || flag) {
+            if (value > M) {
                 lt = mid + 1;
             } else {
                 rt = mid - 1;
