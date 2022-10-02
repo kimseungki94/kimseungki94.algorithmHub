fa96ba4b57875967d7cafd89beb1e1dcd071ffc1
kimseungki <skkim0832@naver.com>
2022-10-02 13:19:42 +0900

    [Silver II] Title: 용돈 관리, Time: 316 ms, Memory: 26964 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, min, sum,max;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        Solution();
+        System.out.println(min);
+    }
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+            max=Math.max(arr[i],max);
+        }
+    }
+
+    public static void Solution() {
+        int lt, rt, mid;
+        lt = max;
+        rt = sum;
+        while (lt <= rt) {
+            mid = (lt + rt) / 2;
+            int value = 1;
+            int countSum = 0;
+            for (int i = 0; i < N; i++) {
+                if (countSum + arr[i] > mid) {
+                    value++;
+                    countSum = 0;
+                }
+                countSum += arr[i];
+            }
+            if (value > M) {
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+                min = mid;
+            }
+        }
+    }
+}
