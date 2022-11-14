8d25acd71812a923a1deb57b56f3698ef2651b33
kimseungki <skkim0832@naver.com>
2022-11-14 09:51:04 +0900

    [level 2] Title: 올바른 괄호, Time: 20.63 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c==')') {
+                if(stack.isEmpty() || stack.peek()==')') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);    
+            }
+        }
+        if(stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
