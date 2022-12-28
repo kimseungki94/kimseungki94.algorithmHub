c9924560ea040023d1cf5b056bc2f4c1c9e181c7
kimseungki <skkim0832@naver.com>
2022-12-28 14:14:37 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.32 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int[] arr = new int[20001];
+    static int count, finalDistance;
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        for(int i=0;i<=n;i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for(int i=0;i<edge.length;i++) {
+            graph.get(edge[i][0]).add(edge[i][1]);
+            graph.get(edge[i][1]).add(edge[i][0]);
+        }
+        
+        BFS(1, 1);
+        for(int i=0;i<=n;i++) {
+            if(arr[i]==finalDistance) answer++;
+        }
+        
+        return answer;
+    }
+    
+    public static void BFS(int start, int init) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        queue.add(init);
+        arr[start]=init;
+        
+        while(!queue.isEmpty()) {
+            int currentNode = queue.poll();
+            int distance = queue.poll();
+            finalDistance=distance;
+            
+            List<Integer> list = graph.get(currentNode);
+            for(int l : list) {
+                if(arr[l]!=0) continue;
+                arr[l]=distance+1;
+                queue.offer(l);
+                queue.offer(distance+1);
+            }
+        }
+    }
+}
