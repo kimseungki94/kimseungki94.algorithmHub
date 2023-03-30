520b320cd63f89c6655d6c72650fa566d094aea5
kimseungki <skkim0832@naver.com>
2023-03-30 16:27:32 +0900

    [level 3] Title: 숫자 게임, Time: 68.25 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -2,29 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
-
-        Arrays.sort(A);
-        Arrays.sort(B);
-        int answer = 0;
+        Arrays.sort(A); //1 3 5 7
+        Arrays.sort(B); // 2 2 6 8
+        int answer=0;
         int index=0;
-        for(int a : A) {
-            if(index==B.length) break;
-            while(true) {
-                if(index==B.length-1) {
-                    if(a<B[index]) answer++;
+        int size = A.length;
+        for(int i=0;i<size;i++) {
+            if(index==size) {
+                break;
+            }
+            while(index!=size) {
+                if(A[i]<B[index]) {
+                    answer++;
                     index++;
                     break;
-                } else {
-                    if(a<B[index]) {
-                        index++;
-                        answer++;
-                        break;
-                    } else {
-                        index++;
-                    }                    
                 }
+                index++;
             }
         }
         return answer;
     }
+    
 }
+/**
+숫자게임진행
+A팀의 출전순서를 보고 B팀이 최대한 승점을 얻는 방법
+
+*/
