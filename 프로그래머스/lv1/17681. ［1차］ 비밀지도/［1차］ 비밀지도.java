707259d833678fc1f8d9494d46d7809e62a2f5ad
kimseungki <skkim0832@naver.com>
2023-04-09 12:39:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.17 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,25 +1,50 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
+        StringBuilder sb = new StringBuilder();
+        String[] map1 = parseDataToBinary(n,arr1);
+        String[] map2 = parseDataToBinary(n,arr2);
+        String[] answer=new String[n];
         for(int i=0;i<n;i++) {
-            String text1 = Integer.toBinaryString(arr1[i]);
-            String text2 = Integer.toBinaryString(arr2[i]);
-            while(text1.length()<n) {
-                text1="0"+text1;
-            }
-            while(text2.length()<n) {
-                text2="0"+text2;
-            }
-            String text3="";
+            String map1Data = map1[i];
+            String map2Data = map2[i];
+            
             for(int j=0;j<n;j++) {
-                if(text1.charAt(j)=='1' || text2.charAt(j)=='1') {
-                    text3+="#";
+                int map1Character = map1Data.charAt(j)-'0';
+                int map2Character = map2Data.charAt(j)-'0';
+                if(map1Character==0 && map1Character==map2Character) {
+                    sb.append(" ");
                 } else {
-                    text3+=" ";
+                    sb.append("#");
                 }
             }
-            answer[i]=text3;           
+            answer[i]=sb.toString();
+            sb.setLength(0);
         }
         return answer;
     }
+    
+    private String[] parseDataToBinary(int n, int[] arr) {
+        String[] data = new String[n];
+        for(int i=0;i<n;i++) {
+            data[i]=Integer.toBinaryString(arr[i]);
+            while(data[i].length()<n) {
+                data[i]="0"+data[i];
+            }
+        }
+        return data;
+    }
 }
+/**
+프로도가 비상금 숨긴 비밀지도
+암호해독
+
+
+n
+
+지도 1
+지도 2
+
+1,2 하나라도 벽이면 전체지도 벽
+1,2 모두 공백이면 공백
+
+*/
